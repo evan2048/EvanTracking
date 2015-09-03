ec08c41ee0446a164cad2128cb5c7712865860d6
evan <gch163mail@163.com>
2015-09-03 13:59:21 +0800

    change tab to 4 space

--- dlib-c/src/EvanTracking-dlib-c.cpp
+++ dlib-c/src/EvanTracking-dlib-c.cpp
@@ -1,3 +1,4 @@
+//note:use 4 space instead tab
 
 #include <dlib/image_processing.h>
 #include <dlib/gui_widgets.h>
@@ -28,30 +29,30 @@ correlation_tracker tracker;
 
 void mouseHandler(int event, int x, int y, int flags, void* param)
 {
-	//if not in select target mode,just ignore and return
-	if(isSelectingTarget == false)
-	{
-		return;
-	}
-	//enter select target mode
-	if(event == CV_EVENT_LBUTTONDOWN && isMouseLeftButtonDown == false)
-	{
-		isMouseLeftButtonDown = true;
-		targetInitStartPoint = cv::Point(x, y);
-	}
-	else if(event == CV_EVENT_MOUSEMOVE && isMouseLeftButtonDown == true)
-	{
-		targetInitEndPoint = cv::Point(x, y);
-		cv::Mat tempFrame = targetFrame.clone();
-		cv::rectangle(tempFrame, targetInitStartPoint, targetInitEndPoint, CV_RGB(255, 0, 0), 1, 8, 0);
-		cv::imshow(mainWindowString, tempFrame);
-	}
-	else if(event == CV_EVENT_LBUTTONUP && isMouseLeftButtonDown == true)
-	{
-		isMouseLeftButtonDown = false;
-		isSelectingTarget = false;
-		targetInitEndPoint = cv::Point(x, y);
-		targetRect = cv::Rect(targetInitStartPoint, targetInitEndPoint);
+    //if not in select target mode,just ignore and return
+    if(isSelectingTarget == false)
+    {
+        return;
+    }
+    //enter select target mode
+    if(event == CV_EVENT_LBUTTONDOWN && isMouseLeftButtonDown == false)
+    {
+        isMouseLeftButtonDown = true;
+        targetInitStartPoint = cv::Point(x, y);
+    }
+    else if(event == CV_EVENT_MOUSEMOVE && isMouseLeftButtonDown == true)
+    {
+        targetInitEndPoint = cv::Point(x, y);
+        cv::Mat tempFrame = targetFrame.clone();
+        cv::rectangle(tempFrame, targetInitStartPoint, targetInitEndPoint, CV_RGB(255, 0, 0), 1, 8, 0);
+        cv::imshow(mainWindowString, tempFrame);
+    }
+    else if(event == CV_EVENT_LBUTTONUP && isMouseLeftButtonDown == true)
+    {
+        isMouseLeftButtonDown = false;
+        isSelectingTarget = false;
+        targetInitEndPoint = cv::Point(x, y);
+        targetRect = cv::Rect(targetInitStartPoint, targetInitEndPoint);
         targetMat = targetFrame(targetRect).clone();
         cv::namedWindow("selectedTarget",cv::WINDOW_AUTOSIZE);
         cv::imshow("selectedTarget",targetMat);
@@ -59,22 +60,22 @@ void mouseHandler(int event, int x, int y, int flags, void* param)
         dlib::cv_image<bgr_pixel> dlibTarget(targetFrame);
         tracker.start_track(dlibTarget, centered_rect(point(targetRect.x + targetRect.width/2, targetRect.y + targetRect.height/2), targetRect.width, targetRect.height));
         isTracking = true;
-	}
+    }
 }
 
 int main(int argc, char** argv)
 {
-	cv::VideoCapture cap;
-	//read param
-	if(argc<2)
-	{
-		printf("error:param error\n");
-		return -1;
-	}
-	else if(argc==2)
-	{
-		cap.open(argv[1]);
-	}
+    cv::VideoCapture cap;
+    //read param
+    if(argc<2)
+    {
+        printf("error:param error\n");
+        return -1;
+    }
+    else if(argc==2)
+    {
+        cap.open(argv[1]);
+    }
     cv::namedWindow(mainWindowString,cv::WINDOW_AUTOSIZE);
     cv::setMouseCallback(mainWindowString, mouseHandler, 0);
     if(!cap.isOpened())
@@ -88,41 +89,41 @@ int main(int argc, char** argv)
         char key = cv::waitKey(waitKeyDelay);
         if(key == 's' && isTracking == false)
         {
-        	isSelectingTarget = true;
-        	targetFrame = frame.clone();
-        	cv::imshow(mainWindowString, targetFrame);
+            isSelectingTarget = true;
+            targetFrame = frame.clone();
+            cv::imshow(mainWindowString, targetFrame);
         }
         else if(key == 'r' && isTracking == true)
         {
-        	isTracking = false;
-        	dlib::cv_image<bgr_pixel> dlibTarget(targetFrame);
-        	tracker.start_track(dlibTarget, centered_rect(point(targetRect.x + targetRect.width/2, targetRect.y + targetRect.height/2), targetRect.width, targetRect.height));
+            isTracking = false;
+            dlib::cv_image<bgr_pixel> dlibTarget(targetFrame);
+            tracker.start_track(dlibTarget, centered_rect(point(targetRect.x + targetRect.width/2, targetRect.y + targetRect.height/2), targetRect.width, targetRect.height));
             isTracking = true;
         }
         else if(key == 'q')
         {
-        	break;
+            break;
         }
         //show preview
         if(isSelectingTarget == false)
         {
-        	cap >> frame;
-        	if(frame.empty())
-        	{
-        		printf("frame empty\n");
-        		break;
-        	}
-        	if(isTracking == true)
-        	{
-        		dlib::cv_image<bgr_pixel> dlibFrame(frame);
-        	    tracker.update(dlibFrame);
-        	    drectangle dlibTargetPosition = tracker.get_position();
-        	    targetCurrentStartPoint = cv::Point(dlibTargetPosition.left(), dlibTargetPosition.top());
-        	    targetCurrentEndPoint = cv::Point(dlibTargetPosition.right(), dlibTargetPosition.bottom());
-        	    cv::rectangle(frame, targetCurrentStartPoint, targetCurrentEndPoint, CV_RGB(0, 0, 255), 2, 8, 0);
-        	    printf("target at:%d %d %d %d\n", targetCurrentStartPoint.x, targetCurrentStartPoint.y, targetCurrentEndPoint.x - targetCurrentStartPoint.x, targetCurrentEndPoint.y - targetCurrentStartPoint.y);
-        	}
-        	cv::imshow(mainWindowString, frame);
+            cap >> frame;
+            if(frame.empty())
+            {
+                printf("frame empty\n");
+                break;
+            }
+            if(isTracking == true)
+            {
+                dlib::cv_image<bgr_pixel> dlibFrame(frame);
+                tracker.update(dlibFrame);
+                drectangle dlibTargetPosition = tracker.get_position();
+                targetCurrentStartPoint = cv::Point(dlibTargetPosition.left(), dlibTargetPosition.top());
+                targetCurrentEndPoint = cv::Point(dlibTargetPosition.right(), dlibTargetPosition.bottom());
+                cv::rectangle(frame, targetCurrentStartPoint, targetCurrentEndPoint, CV_RGB(0, 0, 255), 2, 8, 0);
+                printf("target at:%d %d %d %d\n", targetCurrentStartPoint.x, targetCurrentStartPoint.y, targetCurrentEndPoint.x - targetCurrentStartPoint.x, targetCurrentEndPoint.y - targetCurrentStartPoint.y);
+            }
+            cv::imshow(mainWindowString, frame);
         }
     }
     cap.release();
